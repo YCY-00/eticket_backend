ca5ebc382a9ea08765fe2cee725aa45dbe73996b
se000202 <77709607+se000202@users.noreply.github.com>
2023-06-18 18:11:47 +0900

    Update urls.py

--- backend/notification/urls.py
+++ backend/notification/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     get_notification
-    get_notificationss,
+    get_notifications,
     register_notificaiton,
     update_notificaiton,
     delete_notification,
@@ -12,8 +12,8 @@ app_name = "notification"
 
 urlpatterns = [
     path("register_notificaiton/", register_notification, name="register_notification"),
-    path("update/<str:notification_id>", update_ticket, name="update_notification"),
-    path("get/<str:notification_id>/", get_event, name="get_notification"),
+    path("update/<str:notification_id>", update_notification, name="update_notification"),
+    path("get/<str:notification_id>/", get_notification, name="get_notification"),
     path("get/", get_notificaitons, name="get_notifications"),
     path("delete/<str:notification_id>", delete_notification, name="delete_notification"),
 ]
