a9ec3c7a6e4165b1696da7fcc618a959fced1267
se000202 <77709607+se000202@users.noreply.github.com>
2023-06-18 18:10:16 +0900

    Update urls.py

--- backend/ticket/urls.py
+++ backend/ticket/urls.py
@@ -12,7 +12,7 @@ from .views import (
 app_name = "ticket"
 
 urlpatterns = [
-    path("get_ticket/", get_ticket, name="get_ticket"),
+    path("register_ticket/", register_ticket, name="register_ticket"),
     path("update/<str:ticket_id>", update_ticket, name="update_ticket"),
     path("get/<str:ticket_id>/", get_event, name="get_event"),
     path("get/", get_tickets, name="get_tickets"),
