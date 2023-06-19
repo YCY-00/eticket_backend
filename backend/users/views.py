407b862df80296fb899d45459d2284a7c40e38aa
rs0909 <59247551+rs0909@users.noreply.github.com>
2023-06-19 21:35:32 +0900

    userview added

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,14 +1,23 @@
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView, DeleteView
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+)
+from rest_framework.response import Response
+
+from rest_framework import permissions
+
 
 User = get_user_model()
 
 
-class UserDetailView(DetailView):
+class UserDetailView(LoginRequiredMixin, DetailView):
     model = User
     slug_field = "username"
     slug_url_kwarg = "username"
@@ -17,26 +26,44 @@ class UserDetailView(DetailView):
 user_detail_view = UserDetailView.as_view()
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = User
     fields = ["name"]
     success_message = _("Information successfully updated")
+
+    def get_success_url(self):
+        assert self.request.user.is_authenticated  # for mypy to know that the user is authenticated
+        return self.request.user.get_absolute_url()
+
+    def get_object(self):
+        return self.request.user
 
 
 user_update_view = UserUpdateView.as_view()
 
 
-class UserRedirectView(RedirectView):
+class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
+
+    def get_redirect_url(self):
+        return reverse("users:detail", kwargs={"username": self.request.user.username})
 
 
 user_redirect_view = UserRedirectView.as_view()
 
 
-class UserDeleteView(DeleteView):
+class UserDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = User
     fields = ["is_active"]
     success_message = _("User successfully deleted")
 
+    def get_success_url(self):
+        assert self.request.user.is_authenticated  # for mypy to know that the user is authenticated
+        return self.request.user.get_absolute_url()
+
+    def get_object(self):
+        return self.request.user
+
 
 user_delete_view = UserDeleteView.as_view()
+
